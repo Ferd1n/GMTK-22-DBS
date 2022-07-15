cb337cdca3ff3b961cac863b681009ab41d9a244
Ferd1n <playerjester@gmail.com>
2022-07-15 13:24:26 -0300

    Import preAssets

---
+++ GMTK22/Assets/Assets/Scripts/Grid.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Transform movePoint;
+
+    float moveRate = 0.25f;
+    float canMove = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movePoint.parent = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        if(Time.time > canMove)
+        {
+            if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+            {
+                if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+                {
+                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+
+                else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+                {
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                }
+            }
+
+            canMove = Time.time + moveRate;
+        }
+    }
+}
---
+++ GMTK22/Assets/Assets/Scripts/Platformer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platformer : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float jumpForce = 500f;
+    public bool canJump;
+    public float moveSpeed = 5.0f;
+
+    void Update()
+    {
+        Movement();
+        Jump();
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector2(moveSpeed * Time.deltaTime, 0));
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector2(-moveSpeed * Time.deltaTime, 0));
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))
+        {
+            if (canJump == true)
+            {
+                rb.velocity = Vector2.zero;
+                rb.AddForce(new Vector2(0, jumpForce * Time.fixedDeltaTime), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            canJump = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            canJump = false;
+        }
+    }
+
+
+}

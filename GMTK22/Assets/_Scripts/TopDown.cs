b5d3686104fb55a79066e9745e81e48a41740ec5
Ferd1n <playerjester@gmail.com>
2022-07-15 18:49:16 -0300

    blablabla

--- GMTK22/Assets/_Scripts/TopDown.cs
+++ GMTK22/Assets/_Scripts/TopDown.cs
@@ -8,6 +8,7 @@ public class TopDown : MonoBehaviour
     public Vector2 movement;
     public Rigidbody2D rb;
 
+    public int health = 3;
 
     // Update is called once per frame
     void Update()
@@ -20,4 +21,15 @@ public class TopDown : MonoBehaviour
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
+
+    public void Damage()
+    {
+        health--;
+
+        if(health<1)
+        {
+            Debug.Log("Player died");
+        }
+    }
+
 }
